c1b0ba31d58647a0ee86645d4cbfd08d0b1d466f
p-tvo-hannd <p-tvo-hannd>
2016-09-06 16:57:53 +0700

    fds

--- chapter4/src/index.js
+++ chapter4/src/index.js
@@ -1,23 +1,4 @@
 import React from 'react';
 import BookStore from './BookStore';
 
-var InputExample = React.createClass({
-  getInitialState(){
-    return ({
-      name: '-'
-    });
-  },
-  handleChange(event){
-    this.setState({
-      name: event.target.value
-    });
-  },
-  render: function () {
-    return (
-      <input type="text" value={this.state.name} onChange={this.handleChange}/>
-    );
-  }
-});
-
-
-React.render(<InputExample />, document.getElementById('root'));
+React.render(<BookStore />, document.body);
